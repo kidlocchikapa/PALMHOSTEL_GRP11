cd3215ef94ef88eb8c6644459526438a1d1e288f
“kidlocchikapagit config --global user.name “kidlocchikapa <bed-com-48-44@unima.ac.mw>
2024-05-10 23:39:31 +0200

    Our project_PalmHostel

---
+++ PALMHOSTEL_GRP11/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="flex flex-row w-full h-screen items-center justify-center bg-blue-230">PalmHostel</div>
+  );
+}
+
+export default App;
