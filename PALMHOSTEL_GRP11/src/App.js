53e41401865a3439a57a891b2a3e9e3cd913d2e3
“kidlocchikapagit config --global user.name “kidlocchikapa <bed-com-48-44@unima.ac.mw>
2024-05-22 21:18:16 +0200

    Added project app

--- PALMHOSTEL_GRP11/src/App.js
+++ PALMHOSTEL_GRP11/src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import Navbar from './components/HostelListing/NavBar';
+import HomePageHero from './components/home';
+import Footer from './components/HostelListing/Footer';
+import Cards from './components/HostelListing/Cards';
+import FoodCards from './components/HostelListing/HotelRatingCard';
+import HostelCards from './components/HostelListing/Hostellisting';
+
 
 function App() {
   return (
-    <div className="flex flex-row w-full h-screen items-center justify-center bg-blue-230">PalmHostel</div>
+   <div className="relative">
+    
+    <Navbar/>
+       <HomePageHero/>
+       <Cards/>
+       <HostelCards/>
+       <Footer/>
+   </div>
   );
 }
 
