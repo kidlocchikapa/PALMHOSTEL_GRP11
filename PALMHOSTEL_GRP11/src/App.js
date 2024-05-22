48bb70ec3e6c8db805946d49c7cee934453ec9f1
“kidlocchikapagit config --global user.name “kidlocchikapa <bed-com-48-44@unima.ac.mw>
2024-05-22 21:09:10 +0200

    Added project app

--- PALMHOSTEL_GRP11/src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function App() {
-  return (
-    <div className="flex flex-row w-full h-screen items-center justify-center bg-blue-230">PalmHostel</div>
-  );
-}
-
-export default App;
