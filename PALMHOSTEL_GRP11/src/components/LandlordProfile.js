5018da3be77fd2da3a99de2adee1ecf6502cf556
Bkatete <167067852+Bkatete@users.noreply.github.com>
2024-05-24 00:37:14 +0200

    add landlordprofile component

---
+++ PALMHOSTEL_GRP11/src/components/LandlordProfile.js
@@ -0,0 +1,19 @@
+// src/components/LandlordProfile.jsx
+import React from 'react';
+import LandlordImage from './LandlordImage.jpg';
+
+const LandlordProfile = ({ profile }) => {
+  return (
+    <div className="flex flex-col items-center p-4 bg-gray-300 rounded-lg mb-6">
+      <img 
+        src= {LandlordImage}
+        alt="Profile" 
+        className="w-60 h-60 rounded-20 object-cover mb-4"
+      />
+      <h4 className="text-lg font-bold">{profile.name}</h4>
+      <p className="text-sm text-black">{profile.phone}</p>
+    </div>
+  );
+};
+
+export default LandlordProfile;
